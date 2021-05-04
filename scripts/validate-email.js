71489311c6bcf0db0f24ac793e411cc7b120bc3d
R. Douglas Barbieri <doug@dooglio.net>
2021-05-04 12:39:59 -0500

    Added SSL support.
    The flow is to try non-SSL first, then fall back
    to SSL if the mailbox returned is `null`.

---
+++ scripts/validate-email.js
@@ -0,0 +1,56 @@
+const EmailValidator = require('../lib')
+const logger = require('../lib/logger')
+
+const validator = new EmailValidator({
+  timeout: 10000,
+  fromAddress: 'no-reply@foobar.com',
+  portList: [25, 467, 587]
+})
+
+const isEmailValid = ({ wellFormed, validDomain, validMailbox }) => {
+  const vals = [wellFormed, validDomain, validMailbox]
+  for (const val of vals) {
+    // we ignore null & undefined values -> that means the validator could not perform the check
+    if (val === false) return false
+  }
+  return true
+}
+
+const addresses = [
+  'support@exodus.com',
+  'fverwgerpgkpokgp@exodus.com',
+  'consulting@dooglio.net',
+  'foobar1235@hotmail.com',
+  'fredsbank1234@live.com',
+  'fredsbank1234459345439@yahoo.com',
+  'fredsmith@yahoo.com',
+  'frwjgrg34t4p3kvpkpokapokg@yahoo.com',
+  'dooglio@gmail.com',
+  'fredflinston123465@gmail.com',
+  'foobar@protonmail.com',
+  'foobar1234533353@protonmail.com',
+]
+
+async function validateAll() {
+  for (const address of addresses) {
+    validator.useOpenSSL = false;
+    let result = await validator.verify(address)
+    const valid = isEmailValid(result)
+    logger.info(`address '${address}' done, validMailbox=${result.validMailbox}, validEmail=${valid}`)
+    if (result.validMailbox === null) {
+      validator.useOpenSSL = true;
+      result = await validator.verify(address)
+      logger.info(`address '${address}' done, validMailbox=${result.validMailbox}, validEmail=${valid} [SSL]`)
+    }
+  }
+}
+
+validateAll()
+  .then(() => {
+    process.exit(0)
+  })
+  .catch(err => {
+    logger.error(err)
+    process.exit(1)
+  })
+
